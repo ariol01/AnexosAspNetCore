5d37ebd74d928d9002b58c4aa0f64a41a5a44a52
Ariel Veras <ariolvs123@gmail.com>
2022-02-15 15:26:10 -0400

    listagem de documentos arrumada

--- Infraestrutura/ArquivoContext.cs
+++ Infraestrutura/ArquivoContext.cs
@@ -8,10 +8,10 @@ namespace DSAnexoDocumentoProjeto.Infraestrutura
         {
 
         }
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer(@"Data Source=anexo-documento-db.c5au5rghvszj.sa-east-1.rds.amazonaws.com;Initial Catalog=UploadDB;User=Admin;Password=$dba_ariol;");
-        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=anexo-documento-db.c5au5rghvszj.sa-east-1.rds.amazonaws.com;Database=UploadDB;User=admin;Password=$dba_ariol;");
+        }
 
         public DbSet<AnexoDeDocumento> Anexos { get; set; }
     }
