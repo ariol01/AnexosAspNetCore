4cf5a3125dd92f5c4cabb9aa2aff609eaef3eed3
Ariel Veras <ariolvs123@gmail.com>
2022-02-17 18:45:21 -0400

    projeto em anexo com CRUD. telas de layout em bootstrap 5 , sweetalert, jquery. tela editar modificada.

--- Infraestrutura/ArquivoContext.cs
+++ Infraestrutura/ArquivoContext.cs
@@ -8,11 +8,11 @@ namespace DSAnexoDocumentoProjeto.Infraestrutura
         {
 
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Data Source=SQL5086.site4now.net;Initial Catalog=db_a83110_anexodb;User Id=db_a83110_anexodb_admin;Password=arioldba123");
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(@"");
 
-        }
+        //}
 
         public DbSet<AnexoDeDocumento> Anexos { get; set; }
     }
